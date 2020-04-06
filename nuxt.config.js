b3ac0d1ac5dd1f97f9db0be13bcb2b93b19f059d
Daniela Metodieva <daniela.p.metodieva@gmail.com>
2020-04-06 08:28:21 +0300

    Add loading transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ import modules from 'dev-assets/config/nuxt/modules';
 import build from 'dev-assets/config/nuxt/build';
 import banner from 'dev-assets/config/nuxt/banner';
 import dir from 'cv-assets/config/nuxt/dir';
+import loading from 'cv-assets/config/nuxt/loading';
 import loadingIndicator from 'cv-assets/config/nuxt/loadingIndicator';
 import plugins from 'cv-assets/config/nuxt/plugins';
 import i18nConfig from 'cv-assets/config/nuxt/modules/i18n';
@@ -36,7 +37,7 @@ export default {
   /*
    ** Customize the progress-bar
    */
-  loading: false,
+  loading,
   loadingIndicator,
 
   /*
